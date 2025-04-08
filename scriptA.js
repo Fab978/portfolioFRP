af41f9be8d222a941147b28d6c65e2a08070165f
Fabien <fabien.lemmelet@epita.fr>
2025-04-08 08:54:07 +0200

    Push of files

---
+++ scriptA.js
@@ -0,0 +1,13 @@
+// Script de base pour un scroll fluide entre les sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
